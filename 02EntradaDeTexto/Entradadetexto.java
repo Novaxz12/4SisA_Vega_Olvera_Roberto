f62c1480de78c94b3fa3cab5b0db7a426a6d81a0
Novaxz12 <r.vega.o027@edu.utc.mx>
2023-09-23 11:43:35 -0600

    Entradadetexto

---
+++ 02EntradaDeTexto/Entradadetexto.java
@@ -0,0 +1,40 @@
+//debe de tner una clase
+//public class / class es lo mismo
+
+//variable int num por defecto publico int num
+//libreria
+import java.io.*;
+
+
+class Entradadetexto{
+
+    //debe de existir un metodo principal
+    /*
+     * @param args
+     * @throws IOException
+     */
+    public static void mian (String[]args) throws IOException{}
+    
+    public static void main(String[] arg){
+        //debemos de crear una estructura basica 
+        //declaracion
+        String nombre ;
+        //mensaje para el usuario
+        System.out.print("Por favor ingresa tu nombre");
+        //ln es para salto de linea
+
+        //asignar que operacion o comportamiento le voy a otorgar ala variable
+        //nombre = System.console().read();
+        nombre = System.console().readLine();
+     
+        //System, es la clase del sistema
+        //console() es el metodo que puede obtener los datos de la consola
+        //readLine () es el metodo que puede dar lectura a los datos introducidos en la consola
+
+        System.out.println("Hola " + nombre + " Bienvenido a tronar wiii ");
+
+
+
+    }
+
+} 
